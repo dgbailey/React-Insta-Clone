5011db1977e6eb241f1573bf71c40d5493f757ac
dgbailey <bailey.dust@gmail.com>
2019-03-28 15:24:00 -0700

    comments now styled with new styled component prop comment imported from reusables

---
+++ instagram/dustininsta/src/Components/styles/reusables/UserName.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+
+const UserName = styled.li`
+
+
+margin: ${props => props.banner ? '10px':null};
+font-weight: ${props => props.banner ? 'bold':null};
+font-size: ${props => props.banner ? '15px':null};;
+margin: ${props => props.comment ? '3px':null};
+font-weight: ${props => props.comment ? 'bold':null};
+font-size: ${props => props.comment ? '14px':null};
+`
+
+export default UserName;
+
